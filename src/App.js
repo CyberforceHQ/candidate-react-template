afef7e18e9b8bf417c3d143a8de8e25fef257fa8
Ishan7015 <ip.contactto@gmail.com>
2025-06-27 12:20:56 +0530

    Removed Solution for template

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
-import React, { useState } from 'react';
 import './App.css';
+import React from 'react';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
-
   return (
     <div className="App">
-      <header className="App-header">
-        {isLoggedIn ? (
-          <h1>Welcome!</h1>
-        ) : (
-          <form onSubmit={(e) => { e.preventDefault(); handleLogin(); }}>
-            <h1>Login Form</h1>
-            <input type="email" placeholder="Enter your email" />
-            <br />
-            <label>
-              Password:
-              <input type="password" />
-            </label>
-            <br />
-            <button type="submit">Login</button>
-          </form>
-        )}
-      </header>
+      Candidate Solution Here
     </div>
   );
 }
